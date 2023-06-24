9fcc29328dd98e86da2a527efd925a8ae956acf2
Frederik Tobner <masterhorst90@web.de>
2023-06-25 00:06:03 +0200

    removed end to end tests

--- src/main.zig
+++ src/main.zig
@@ -24,159 +24,56 @@ pub fn main() u8 {
         return @enumToInt(SysExits.EX_OK);
     } else |err| {
         switch (err) {
-            InterpreterError.COMPILE_ERROR => {
-                std.debug.print("Compile error\n", .{});
-                return @enumToInt(SysExits.EX_DATAERR);
-            },
-            InterpreterError.RUNTIME_ERROR => {
-                std.debug.print("Runtime error\n", .{});
-                return @enumToInt(SysExits.EX_SOFTWARE);
-            },
-            error.AccessDenied => {
-                std.debug.print("Access denied\n", .{});
-                return @enumToInt(SysExits.EX_NOPERM);
-            },
-            error.BadPathName => {
-                std.debug.print("Bad path name\n", .{});
-                return @enumToInt(SysExits.EX_NOINPUT);
-            },
-            error.BrokenPipe => {
-                std.debug.print("Broken pipe\n", .{});
-                return @enumToInt(SysExits.EX_IOERR);
-            },
-            error.ConnectionResetByPeer => {
-                std.debug.print("Connection reset\n", .{});
-                return @enumToInt(SysExits.EX_IOERR);
-            },
-            error.ConnectionTimedOut => {
-                std.debug.print("Connection timed out\n", .{});
-                return @enumToInt(SysExits.EX_IOERR);
-            },
-            error.DeviceBusy => {
-                std.debug.print("Device busy\n", .{});
-                return @enumToInt(SysExits.EX_IOERR);
-            },
-            error.DiskQuota => {
-                std.debug.print("Disk quota exceeded\n", .{});
-                return @enumToInt(SysExits.EX_IOERR);
-            },
-            error.EndOfStream => {
-                std.debug.print("End of stream\n", .{});
-                return @enumToInt(SysExits.EX_IOERR);
-            },
-            error.FileBusy => {
-                std.debug.print("File busy\n", .{});
-                return @enumToInt(SysExits.EX_IOERR);
-            },
-            error.FileLocksNotSupported => {
-                std.debug.print("File locks not supported\n", .{});
-                return @enumToInt(SysExits.EX_IOERR);
-            },
-            error.FileNotFound => {
-                std.debug.print("File not found\n", .{});
-                return @enumToInt(SysExits.EX_NOINPUT);
-            },
-            error.FileTooBig => {
-                std.debug.print("File too big\n", .{});
-                return @enumToInt(SysExits.EX_IOERR);
-            },
-            error.InputOutput => {
-                std.debug.print("Input/Output error\n", .{});
-                return @enumToInt(SysExits.EX_IOERR);
-            },
-            error.InvalidHandle => {
-                std.debug.print("Invalid handle\n", .{});
-                return @enumToInt(SysExits.EX_IOERR);
-            },
-            error.InvalidUtf8 => {
-                std.debug.print("Invalid UTF-8\n", .{});
-                return @enumToInt(SysExits.EX_DATAERR);
-            },
-            error.IsDir => {
-                std.debug.print("Is a directory\n", .{});
-                return @enumToInt(SysExits.EX_NOINPUT);
-            },
-            error.LockViolation => {
-                std.debug.print("Lock violation\n", .{});
-                return @enumToInt(SysExits.EX_IOERR);
-            },
-            error.NameTooLong => {
-                std.debug.print("Name too long\n", .{});
-                return @enumToInt(SysExits.EX_DATAERR);
-            },
-            error.NoDevice => {
-                std.debug.print("No device\n", .{});
-                return @enumToInt(SysExits.EX_IOERR);
-            },
-            error.NoSpaceLeft => {
-                std.debug.print("No space left\n", .{});
-                return @enumToInt(SysExits.EX_IOERR);
-            },
-            error.NotDir => {
-                std.debug.print("Not a directory\n", .{});
-                return @enumToInt(SysExits.EX_NOINPUT);
-            },
-            error.NotOpenForWriting => {
-                std.debug.print("Not open for writing\n", .{});
-                return @enumToInt(SysExits.EX_IOERR);
-            },
-            error.NotOpenForReading => {
-                std.debug.print("Not open for reading\n", .{});
-                return @enumToInt(SysExits.EX_IOERR);
-            },
-            error.OutOfMemory => {
-                std.debug.print("Out of memory\n", .{});
-                return @enumToInt(SysExits.EX_OSERR);
-            },
-            error.OperationAborted => {
-                std.debug.print("Operation aborted\n", .{});
-                return @enumToInt(SysExits.EX_IOERR);
-            },
-            error.PathAlreadyExists => {
-                std.debug.print("Path already exists\n", .{});
-                return @enumToInt(SysExits.EX_CANTCREAT);
-            },
-            error.PipeBusy => {
-                std.debug.print("Pipe busy\n", .{});
-                return @enumToInt(SysExits.EX_IOERR);
-            },
-            error.ProcessFdQuotaExceeded => {
-                std.debug.print("Process file descriptor quota exceeded\n", .{});
-                return @enumToInt(SysExits.EX_IOERR);
-            },
-            error.SharingViolation => {
-                std.debug.print("Sharing violation\n", .{});
-                return @enumToInt(SysExits.EX_IOERR);
-            },
-            error.StreamTooLong => {
-                std.debug.print("Stream too long\n", .{});
-                return @enumToInt(SysExits.EX_IOERR);
-            },
-            error.SymLinkLoop => {
-                std.debug.print("Symbolic link loop\n", .{});
-                return @enumToInt(SysExits.EX_IOERR);
-            },
-            error.SystemResources => {
-                std.debug.print("System resource exhausted\n", .{});
-                return @enumToInt(SysExits.EX_OSERR);
-            },
-            error.SystemFdQuotaExceeded => {
-                std.debug.print("System file descriptor quota exceeded\n", .{});
-                return @enumToInt(SysExits.EX_IOERR);
-            },
-            error.Unexpected => {
-                std.debug.print("Unexpected error\n", .{});
-                return @enumToInt(SysExits.EX_SOFTWARE);
-            },
-            error.Unseekable => {
-                std.debug.print("Unseekable\n", .{});
-                return @enumToInt(SysExits.EX_IOERR);
-            },
-            error.WouldBlock => {
-                std.debug.print("Would block\n", .{});
-                return @enumToInt(SysExits.EX_IOERR);
-            },
+            InterpreterError.COMPILE_ERROR => std.debug.print("Compile error\n", .{}),
+            InterpreterError.RUNTIME_ERROR => std.debug.print("Runtime error\n", .{}),
+            .AccessDenied => std.debug.print("Access denied\n", .{}),
+            .BadPathName => std.debug.print("Bad path name\n", .{}),
+            .BrokenPipe => std.debug.print("Broken pipe\n", .{}),
+            .ConnectionResetByPeer => std.debug.print("Connection reset\n", .{}),
+            .ConnectionTimedOut => std.debug.print("Connection timed out\n", .{}),
+            .DeviceBusy => std.debug.print("Device busy\n", .{}),
+            .DiskQuota => std.debug.print("Disk quota exceeded\n", .{}),
+            .EndOfStream => std.debug.print("End of stream\n", .{}),
+            .FileBusy => std.debug.print("File busy\n", .{}),
+            .FileLocksNotSupported => std.debug.print("File locks not supported\n", .{}),
+            .FileNotFound => std.debug.print("File not found\n", .{}),
+            .FileTooBig => std.debug.print("File too big\n", .{}),
+            .InputOutput => std.debug.print("Input/Output error\n", .{}),
+            .InvalidHandle => std.debug.print("Invalid handle\n", .{}),
+            .InvalidUtf8 => std.debug.print("Invalid UTF-8\n", .{}),
+            .IsDir => std.debug.print("Is a directory\n", .{}),
+            .LockViolation => std.debug.print("Lock violation\n", .{}),
+            .NameTooLong => std.debug.print("Name too long\n", .{}),
+            .NoDevice => std.debug.print("No device\n", .{}),
+            .NoSpaceLeft => std.debug.print("No space left\n", .{}),
+            .NotDir => std.debug.print("Not a directory\n", .{}),
+            .NotOpenForWriting => std.debug.print("Not open for writing\n", .{}),
+            .NotOpenForReading => std.debug.print("Not open for reading\n", .{}),
+            .OutOfMemory => std.debug.print("Out of memory\n", .{}),
+            .OperationAborted => std.debug.print("Operation aborted\n", .{}),
+            .PathAlreadyExists => std.debug.print("Path already exists\n", .{}),
+            .PipeBusy => std.debug.print("Pipe busy\n", .{}),
+            .ProcessFdQuotaExceeded => std.debug.print("Process file descriptor quota exceeded\n", .{}),
+            .SharingViolation => std.debug.print("Sharing violation\n", .{}),
+            .StreamTooLong => std.debug.print("Stream too long\n", .{}),
+            .SymLinkLoop => std.debug.print("Symbolic link loop\n", .{}),
+            .SystemResources => std.debug.print("System resource exhausted\n", .{}),
+            .SystemFdQuotaExceeded => std.debug.print("System file descriptor quota exceeded\n", .{}),
+            .Unexpected => std.debug.print("Unexpected error\n", .{}),
+            .Unseekable => std.debug.print("Unseekable\n", .{}),
+            .WouldBlock => std.debug.print("Would block\n", .{}),
         }
+        return switch (err) {
+            InterpreterError.COMPILE_ERROR => @enumToInt(SysExits.EX_DATAERR),
+            .AccessDenied => @enumToInt(SysExits.EX_NOPERM),
+            .FileNotFound => @enumToInt(SysExits.EX_NOINPUT),
+            .InvalidUtf8, .NameTooLong => @enumToInt(SysExits.EX_DATAERR),
+            .NotDir => @enumToInt(SysExits.EX_NOINPUT),
+            .PathAlreadyExists => @enumToInt(SysExits.EX_CANTCREAT),
+            .OutOfMemory, .OperationAborted.SystemResources => @enumToInt(SysExits.EX_OSERR),
+            .BadPathName, .BrokenPipe, .ConnectionResetByPeer, .ConnectionTimedOut, .DeviceBusy, .DiskQuota, .EndOfStream, .FileBusy, .FileLocksNotSupported, .FileTooBig, .InputOutput, .InvalidHandle, .IsDir, .LockViolation, .NoDevice, .NoSpaceLeft, .NotOpenForWriting, .NotOpenForReading, .PipeBusy, .ProcessFdQuotaExceeded, .SharingViolation, .StreamTooLong, .SymLinkLoop.SymLinkLoop, .SystemFdQuotaExceeded, .Unseekable, .WouldBlock => @enumToInt(SysExits.EX_IOERR),
+            InterpreterError.RUNTIME_ERROR, .Unexpected => @enumToInt(SysExits.EX_SOFTWARE),
+        };
     }
 }
 
--- test.zig
+++ test.zig
@@ -3,5 +3,4 @@ comptime {
     _ = @import("src/token.zig");
     _ = @import("src/fnv1a.zig");
     _ = @import("src/table.zig");
-    _ = @import("src/e2e_test.zig");
 }
