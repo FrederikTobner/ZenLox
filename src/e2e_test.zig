ad6ab2025767122804f3b26ff15a120a5f2b67d5
Frederik Tobner <masterhorst90@web.de>
2023-06-24 23:32:38 +0200

    testing if we can print at all

--- src/e2e_test.zig
+++ src/e2e_test.zig
@@ -8,7 +8,7 @@ fn e2e_test(input: []const u8, expected_output: []const u8) !void {
     var general_purpose_allocator = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = general_purpose_allocator.allocator();
     defer _ = general_purpose_allocator.deinit();
-    writer.print("test", {});
+    try writer.print("test", .{});
     var vm = VirtualMachine.init(&writer, allocator);
     defer vm.deinit();
     try vm.interpret(input);
@@ -16,8 +16,8 @@ fn e2e_test(input: []const u8, expected_output: []const u8) !void {
     try source.seekTo(0);
     const read = try source.read(&buf);
     // Ignoring the last byte because it is a new line
-    try std.testing.expectEqualStrings(expected_output[0 .. read - 1], buf[0 .. read - 1]);
     try std.fs.cwd().deleteFile("output.txt");
+    try std.testing.expectEqualStrings(expected_output[0..expected_output.len], buf[0 .. read - 1]);
 }
 
 test "Redirect IO" {
